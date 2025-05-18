3d65532726c189db1751be1f7b70d704d8b4fd08
Joao Masumoto <jgabriel.mp@hotmail.com>
2025-05-18 00:41:06 -0300

    Adiciona tratamento de exceção no par ou impar

--- src/main/java/application/ParOuImpar.java
+++ src/main/java/application/ParOuImpar.java
@@ -1,13 +1,28 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ParOuImpar {
     public static void executar() {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("======PAR OU IMPAR======");
-        System.out.print("Escolha um número: ");
-        int num = sc.nextInt();
+
+        boolean numeroValido = false;
+        int num = 0;
+        while (!numeroValido) {
+            try {
+                System.out.print("Escolha um número: ");
+                num = sc.nextInt();
+                numeroValido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Insira apenas números.");
+                sc.nextLine();
+            }
+        }
+
+        System.out.println();
 
         if (ehPar(num)) {
             System.out.printf("O número %d é par.", num);
